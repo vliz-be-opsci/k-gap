038054689cc3303198b81791790c9738d5786402
Marc Portier <marc.portier@gmail.com>
2024-02-28 17:11:16 +0100

    kgap jupyter image to handle locally defined /notebooks/requirements.txt
    fixes #7

--- jupyter/kgap/entrypoint-wrap.sh
+++ jupyter/kgap/entrypoint-wrap.sh
@@ -2,7 +2,8 @@
 # 0. config reading from environment
 
 
-# 1. merge in some useful kgap stuff (if it does not exist)
+# 1. intercept the original entry point in a number of interesting ways
+# 1.a merge in some useful kgap stuff (if it does not exist)
 SRC_FOLDER=${SRC_FOLDER:-/kgap/notebooks}
 TGT_FOLDER=/notebooks
 echo "merging ${SRC_FOLDER} to ${TGT_FOLDER}"
@@ -11,14 +12,21 @@ echo "merging ${SRC_FOLDER} to ${TGT_FOLDER}"
 # -v == verbose, shows what is actually done
 cp -R -n -v ${SRC_FOLDER}/* ${TGT_FOLDER}
 
+# 1.b link the /data and /config volumes under /notebooks for easy access
 for lnk in data config; do
   echo "checking link for ${lnk}"
-  if [ -d /${lnk} ]; then                    # if these exist at the root
+  if [ -d /${lnk} ]; then                  # if these exist at the root
     ln -s /${lnk} ${TGT_FOLDER}/${lnk}     #   then symlink under target
-  else                                       # if not
+  else                                     # if not
     rm -rf ${TGT_FOLDER}/${lnk}            #   be sure to remove the link again
   fi
 done
+
+# 1.c add any suggested pip requirements
+
+if [ -f /notebooks/requirements.txt ]; then
+  pip install -r /notebooks/requirements.txt || true # the || true ensures the script continues in case of errors
+fi
 
 # 2. delegate to the original entrypoint command 
 # note: the underlying entrypoint was detected using:
