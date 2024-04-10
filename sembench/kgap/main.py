9efd104fd3c250f9210091781491b0128c1d7a7a
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2024-04-10 13:36:20 +0200

    Update Dockerfile to use kgap directory for main.py and requirements.txt

---
+++ sembench/kgap/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+from pathlib import Path
+from pysembench.core import Sembench
+
+sb = Sembench(
+    input_data_location=os.getenv("INPUT_DATA_LOCATION"),
+    output_data_location=os.getenv("OUTPUT_DATA_LOCATION"),
+    sembench_data_location=os.getenv("SEMBENCH_DATA_LOCATION"),
+    sembench_config_path=os.getenv("SEMBENCH_CONFIG_PATH"),
+    scheduler_interval_seconds=os.getenv("SCHEDULER_INTERVAL_SECONDS"),
+)
+
+sb.process()
