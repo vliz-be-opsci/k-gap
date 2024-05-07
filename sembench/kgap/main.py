f8cb61a1c889fdb579e9ac3c055219d0227391b6
Marc Portier <marc.portier@gmail.com>
2024-05-07 18:31:53 +0200

    fixes #33

--- sembench/kgap/main.py
+++ sembench/kgap/main.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 import os
 from pathlib import Path
-from pysembench.core import Sembench
+from pysembench.core import Sembench, locations_from_environ
+
 
 sb = Sembench(
-    input_data_location=os.getenv("INPUT_DATA_LOCATION"),
-    output_data_location=os.getenv("OUTPUT_DATA_LOCATION"),
-    sembench_data_location=os.getenv("SEMBENCH_DATA_LOCATION"),
+    locations=locations_from_environ(),
     sembench_config_path=os.getenv("SEMBENCH_CONFIG_PATH"),
     scheduler_interval_seconds=os.getenv("SCHEDULER_INTERVAL_SECONDS"),
 )
