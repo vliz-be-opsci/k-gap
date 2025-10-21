5d1fb8d924a0b815dac2169f749c32f9ab933d9e
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2025-10-21 06:58:13 +0000

    Update health check URI to use the repositories endpoint

--- graphdb/kgap/healthy.sh
+++ graphdb/kgap/healthy.sh
@@ -2,7 +2,7 @@
 # 0. config reading from environment
 REPONAME=${GDB_REPO:-kgap}
 # Use a lightweight endpoint that doesn't scale with triple count
-HEALTH_CHECK_URI="http://localhost:7200/rest/monitor/health" 
+HEALTH_CHECK_URI="http://localhost:7200/rest/repositories" 
 # 1. do the health-check
 #   IMPORTANT NOTE -- being last statement this will produce bash exit code
 curl --fail -X GET --url ${HEALTH_CHECK_URI}  
